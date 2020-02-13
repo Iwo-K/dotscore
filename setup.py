aedf306746d7b47e1ca6b156b2e7860b98c46e60
idk25 <iwo.kucinski@gmail.com>
2020-02-13 18:33:23 +0000

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dotscore',
+    version='0.0.1',
+    description='DoT-score aid interpretation of transcriptomic changes using scRNA-Seq landscapes as references',
+    license='BSD3',
+    packages=['dotscore'],
+    author='Iwo Kucinski',
+    author_email='',
+    url='https://github.com/Iwo-K/dotscore'
+)
